c7f4a51c259a4061f6c9b50d375417b6a2e988ea
Zach Aller <zachaller@users.noreply.github.com>
2021-03-25 09:05:37 -0500

    Update pinniped-cli.rb

--- pinniped-cli.rb
+++ pinniped-cli.rb
@@ -5,7 +5,7 @@ class PinnipedCli < Formula
         tag:      "v0.6.0",
         revision: "f015ad5852805d6d5685b500b46889acb223e8e8"
     license "Apache-2.0"
-    head "https://github.com/vmware-tanzu/pinniped.git", branch: "main"
+    head "https://github.com/vmware-tanzu/pinniped.git", branch: "impersonation-proxy"
     
     depends_on "go" => :build
     depends_on "libtool"
